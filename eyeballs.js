c0c2bfa2263649218d4a3f430aa812cfd0bfb610
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2018-02-19 19:34:22 -0500

    reorganize eyeballs and pupils into global layers list

--- eyeballs.js
+++ eyeballs.js
@@ -7,6 +7,9 @@ var GROUND_COLOR = [253, 125, 152];
 var FOG_START_COLOR = [255, 255, 255, 200];
 var FOG_END_COLOR = [255, 255, 255, 0];
 
+var horizon = CANVAS_HEIGHT / 3 + 30;
+var fogStart = horizon - 40;
+
 var SETTINGS = {
     'maxHeight':        CANVAS_HEIGHT,
     'maxWidth':         CANVAS_WIDTH,
@@ -20,15 +23,11 @@ var SETTINGS = {
     'scaleFactor':      1.6
 };
 
-var drawCanvas;
-
 var eyeballs = [];
 var eyeballPool = eyeballs;
 var pupils = [];
 
-var eyeballInstances = [];
-var pupilInstances = [];
-var fogLines = [];
+var layers = []; // each element is object {height: float, eyeballInstances:[], pupilInstances: []}
 
 function loadEyeballs(){
     var imgNums = ['01', '02', '03', '04', '05'];
@@ -47,11 +46,14 @@ function loadEyeballs(){
 function loadPupils(){
     var imgNums = ['01', '02', '03', '04'];
     for (var i = 0; i < imgNums.length; i++){
-        pupils.push(loadImage(IMGPATH + 'pupil' + imgNums[i] + '.png'));
+        pupils.push({
+            'id': i,
+            'img': loadImage(IMGPATH + 'pupil' + imgNums[i] + '.png')
+        });
     }
 }
 
-function drawBackground(heightStart, heightEnd, colorStart, colorEnd) {
+function drawFog(heightStart, heightEnd, colorStart, colorEnd) {
     noFill();
     for (var i = heightStart; i <= heightEnd; i++) {
         var inter = map(i, heightStart, heightEnd, 0, 2);
@@ -61,21 +63,37 @@ function drawBackground(heightStart, heightEnd, colorStart, colorEnd) {
     }
 }
 
-function drawEyeballs(horizon, fogStart) {
+function createEyeballInstances() {
+
+    var heightInterval = SETTINGS.heightInterval;
+    var heightOffset = SETTINGS.heightOffset;
+    var widthInterval = SETTINGS.widthInterval;
+    var widthOffset = SETTINGS.widthOffset;
+    var scaleValue = SETTINGS.scaleValue;
+
     var loopCount = 0;
     var height = horizon;
     while (height < SETTINGS.maxHeight){
+        var layer = {
+            'height': height,
+            'eyeballInstances': [],
+            'pupilInstances': []
+        };
+        layers.push(layer);
 
         var width = random(-20, 20);
         // don't draw the first row because you can't see it through fog anyway
         while (loopCount != 0 && width < SETTINGS.maxWidth){   
-            var x = width + random(-SETTINGS.widthOffset, SETTINGS.widthOffset);
-            var y = height + random(-SETTINGS.heightOffset); 
-            drawEyeball(x, y, SETTINGS.scaleValue);
-            width += SETTINGS.widthInterval;
+            var x = width + random(-widthOffset, widthOffset);
+            var y = height + random(-heightOffset); 
+            var eyeballInstance = createEyeballInstance(x, y, scaleValue);
+            var pupilInstance = createPupilInstance(eyeballInstance);
+            layer.eyeballInstances.push(eyeballInstance);
+            layer.pupilInstances.push(pupilInstance);
+            width += widthInterval;
         }
 
-        height += SETTINGS.heightInterval;
+        height += heightInterval;
         heightInterval *= SETTINGS.heightFactor;
         heightOffset *= SETTINGS.heightFactor;
         widthInterval *= SETTINGS.widthFactor;
@@ -94,20 +112,19 @@ function getRandomEyeball(){
     return eyeball;
 }
 
-function drawEyeball(x, y, scaleValue){
+function createEyeballInstance(x, y, scaleValue){
     var eyeball = getRandomEyeball();
     var width = eyeball.img.width * scaleValue;
     var height = eyeball.img.height * scaleValue;
     var y = y - height;
-    image(eyeball.img, x, y, width, height);
-    eyeballInstances.push({
+    return {
         'id': eyeball.id,
         'x': x,
         'y': y,
         'width': width,
         'height': height,
         'scale': scaleValue
-    });
+    };
 }
 
 function getPupilYOffset(height, eId){
@@ -134,20 +151,46 @@ function getPupilScale(eId) {
     }
 }
 
-function drawPupils(){
+function createPupilInstance(eyeballInstance) {
     var pupil = random(pupils);
-    var pupilYOffset = getPupilYOffset(height, eyeball.id);
-    var pupilXOffset = getPupilXOffset(width, eyeball.id)
-    var pupilScale = getPupilScale(eyeball.id);
-    image(pupil, x + pupilXOffset, y + pupilYOffset, pupilScale * pupil.width * scaleValue, pupilScale * pupil.height * scaleValue);
-
+    var pupilYOffset = getPupilYOffset(eyeballInstance.height, eyeballInstance.id);
+    var pupilXOffset = getPupilXOffset(eyeballInstance.width, eyeballInstance.id)
+    var pupilScale = getPupilScale(eyeballInstance.id) * eyeballInstance.scale;
+    return {
+        'id': pupil.id,
+        'x': eyeballInstance.x + pupilXOffset,
+        'y': eyeballInstance.y + pupilYOffset,
+        'width': pupilScale * pupil.img.width,
+        'height': pupilScale * pupil.img.height
+    };
 }
 
-function drawFog(){
-     // don't fog the ones in the front
-     if (height < maxHeight) {
-        drawBackground(fogStart, height, color(FOG_START_COLOR), color(FOG_END_COLOR));
+function drawEyeball(eyeballInstance) {
+    var eyeballImg = eyeballs[eyeballInstance.id].img;
+    image(eyeballImg, eyeballInstance.x, eyeballInstance.y, eyeballInstance.width, eyeballInstance.height);
+}
+
+function drawPupil(pupilInstance) {
+    var pupilImg = pupils[pupilInstance.id].img;
+    image(pupilImg, pupilInstance.x, pupilInstance.y, pupilInstance.width, pupilInstance.height);
+}
+
+function drawEyeballs(){
+    for (var i = 0; i < layers.length; i++){
+        var layer = layers[i];
+        var j;
+        for (j = 0; j < layer.eyeballInstances.length; j++){
+            drawEyeball(layer.eyeballInstances[j]);
+        }
+        for (j = 0; j < layer.pupilInstances.length; j++){
+            drawPupil(layer.pupilInstances[j]);
+        }
+        // don't fog the ones in the front
+        if (i < layers.length - 1){
+            drawFog(fogStart, layer.height, color(FOG_START_COLOR), color(FOG_END_COLOR));
+        }
     }
+        
 }
 
 function preload(){
@@ -161,11 +204,12 @@ function preload(){
 function setup(){
 
     createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-    drawCanvas = createGraphics(CANVAS_WIDTH, CANVAS_HEIGHT);
+    createEyeballInstances();     
+    
+}
 
-    var horizon = CANVAS_HEIGHT / 3 + 30;
-    var fogStart = horizon - 40;
-
+function draw() {
+    clear();
     background(color(GROUND_COLOR));
 
     noStroke();
@@ -174,12 +218,7 @@ function setup(){
     fill(topColor);
     rect(0, 0, CANVAS_WIDTH, fogStart + 1);
     noFill();
-    
-    drawEyeballs(horizon, fogStart);     
-    
-}
 
-function draw() {
-    image(drawCanvas, 0, 0);
-    drawPupils();
+    drawEyeballs();
+
 }
